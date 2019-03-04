b36313b909d29b63f57f1ab55b88e65821d49b0c
TichiOigara <sheilanyatichi@gmail.com>
2019-03-04 16:46:57 +0300

    add function for switching between players.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function Player(tempScore, totalScore, rolledNumbers) {
+function Player(rolledNumbers, tempScore, totalScore) {
   this.rolledNumbers = rolledNumbers
   this.tempScore = tempScore;
   this.totalScore = totalScore;
@@ -12,14 +12,21 @@ Player.prototype.addTempScore = function() {
   return this.tempScore;
 }
 Player.prototype.addTotalScore = function() {
-  return this.totalScore += this.tempScore;
+  this.totalScore += this.tempScore;
+  return this.totalScore;
 }
 
 function rollDice() {
   var randomNumber = Math.floor((Math.random() * 6) + 1);
   return randomNumber;
 }
-
+//function for changing player
+function changePlayer() {
+  $("#rollBtn1").toggle();
+  $("#holdBtn1").toggle();
+  $("#rollBtn2").toggle();
+  $("#holdBtn2").toggle();
+}
 var rolledNumbers1 = [];
 var rolledNumbers2 = [];
 var player1 = new Player(0, 0, 0);
@@ -28,82 +35,63 @@ $(document).ready(function() {
 
   $("#rollBtn1").click(function(event) {
     event.preventDefault();
-    //rolledNumbers = [0];
     var rolledNumber = rollDice();
     $("#diceNumber").text(rolledNumber);
     if (rolledNumber != 1) {
       rolledNumbers1.push(rolledNumber);
       player1.rolledNumbers = rolledNumbers1;
       player1.addTempScore();
-      player1.addTotalScore();
       $("#roundResult1").text(player1.tempScore);
-      if (player1.totalScore >= 100) {
-        $("#totalResult1").text(player1.addTotalScore());
-        alert("Player one has won");
-      }
     } else {
-
       player1.rolledNumbers = 0;
       rolledNumbers1 = [0];
-      $("#roundResult1").text(player1.addTempScore());
-      $("#totalResult1").text(player1.addTotalScore());
-      //alert(rolledNumbers1);
-      $("#rollBtn1").hide();
-      $("#holdBtn1").hide();
-      alert("It is player 2 's turn'");
-      $("#rollBtn2").show();
-      $("#holdBtn2").show();
-
+      player1.addTempScore();
+      $("#roundResult1").text(player1.add);
+      $("#totalResult1").text(player1.totalScore);
+      alert("You rolled 1. It is player 2's turn'");
+      changePlayer();
     }
 
 
   });
+
   $("#holdBtn1").click(function(event) {
     event.preventDefault();
-    alert(player1.tempScore);
-    $("#totalResult1").text(player1.totalScore);
+    $("#totalResult1").text(player1.addTotalScore());
+    if (player1.totalScore >= 20) {
+      alert("Player 1 has won the game. Your Score is ");
+    }
     $("#roundResult1").text(0);
     rolledNumbers1 = [0];
-    $("#rollBtn1").hide();
-    $("#holdBtn1").hide();
-    $("#rollBtn2").show();
-    $("#holdBtn2").show();
+    changePlayer();
   });
 
   $("#rollBtn2").click(function(event) {
     event.preventDefault();
-    //alert(player2.tempScore);
-    //rolledNumbers = [0];
+
     var rolledNumber = rollDice();
     $("#diceNumber").text(rolledNumber);
     if (rolledNumber != 1) {
       rolledNumbers2.push(rolledNumber);
       player2.rolledNumbers = rolledNumbers2;
       $("#roundResult2").text(player2.addTempScore());
-      //   //alert(player1.addTotalScore());
     } else {
-
       player2.rolledNumbers = 0;
       $("#roundResult2").text(player2.addTempScore());
       $("#totalResult2").text(player2.addTotalScore());
-      //alert(rolledNumbers2);
-      $("#rollBtn2").hide();
-      $("#holdBtn2").hide();
-      alert("It is player 1 's turn'");
-      $("#rollBtn1").show();
-      $("#holdBtn1").show();
+      alert("You rolled 1.It is player 1 's turn'");
+      changePlayer();
       rolledNumbers2 = [0];
     }
-
-
   });
+
   $("#holdBtn2").click(function(event) {
     $("#totalResult2").text(player2.addTotalScore());
     $("#roundResult2").text(0);
+    if (player2.totalScore >= 20) {
+      alert("Player2 has won the game. Your score is "+ player2.totalScore);
+    }
     rolledNumbers2 = [0];
-    $("#rollBtn2").hide();
-    $("#holdBtn2").hide();
-    $("#rollBtn1").show();
-    $("#holdBtn1").show();
+    changePlayer();
   });
 });
